697f7eb8d092e57c225edff6a5142aa1c91a428f
Lucas Uanderson <lucasuanderson159@live.com>
2025-08-04 15:25:59 -0300

    Update

--- config.py
+++ config.py
@@ -1,21 +1,24 @@
-import os 
+import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config: 
+class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'segredo-desenvolvimento'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
     DEBUG = True
 
+
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    DEBUG = False
+    uri = os.environ.get('DATABASE_URL')
     
-
-
-
-
+    # Render costuma usar postgres://, precisamos adaptar para o novo formato
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql+psycopg://", 1)
+        
+    SQLALCHEMY_DATABASE_URI = uri
+    DEBUG = False
