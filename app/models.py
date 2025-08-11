dedba9b5216f407ba728baf966c441bde51cccca
Lucas Uanderson <lucasuanderson159@live.com>
2025-08-11 12:50:00 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(64), unique=True, nullable = False)
-    password = db.Column(db.String(128), nullable = False)
+    password = db.Column(db.String(255), nullable = False)
     tasks = db.relationship('Task', backref='owner', lazy= True)
 
     def __repr__(self):
